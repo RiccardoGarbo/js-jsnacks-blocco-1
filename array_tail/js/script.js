4c5e71ba7aedd4a123a1fe553caa1b42712d09a6
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2023-12-22 18:06:02 +0100

    verification question

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -3,6 +3,7 @@
 1- chiedendo all'utente quanti elementi dovrà contenere 
 2- verifico che il valore datomi sia valido
 3- creo i numeri casuali
+4- stampo gli ultimi 5
 
 
 */
@@ -16,12 +17,5 @@ let numeroElementi = parseInt(prompt('Quanti elementi devo inserire ?'));
 // Verifica se l'input è un numero valido
 if (isNaN(numeroElementi) || numeroElementi <= 0) {
     console.log('Inserisci un numero valido.');
-} else {
-    // Creazione dell'array 
-    let numeriCasuali = [];
-    for (let i = 0; i < numeroElementi; i++) {
-        //Generazione dei numeri casuali
-        numeriCasuali.push = (Math.floor(Math.random() * 100) + 1);
-        console.log(numeriCasuali); 4
-    }
+
 }
